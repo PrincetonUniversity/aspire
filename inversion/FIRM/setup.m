ef5b59df9a6888c2ca8b963c420100bda72a9e93
Lanhui Wang <lanhuiw@fine221j.math.Princeton.EDU>
2013-08-09 14:26:33 -0400

    Change paths.

--- inversion/FIRM/setup.m
+++ inversion/FIRM/setup.m
@@ -1,6 +1,9 @@
 % add paths for FIRM
 addpath ./FIRM
 %fprintf('Please download the package of irt following the instruction in setup.m!\n')
-% Download irt from http://www.eecs.umich.edu/~fessler/code/index.html
+% The package irt is downloaded from
+% http://www.eecs.umich.edu/~fessler/code/index.html.
+% This software was developed at the University of Michigan by Jeff Fessler and his students. 
 % addpath 'the directory of irt'
+addpath ./irt
 run ./irt/setup.m
