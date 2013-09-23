3faff091b3169927736019750c0e5f5384ebc26a
Yoel Shkolnisky <yoel@yoeldesktop1.(none)>
2013-09-23 20:37:19 +0300

    Move FIRM folder and create initpath
    1. Move FIRM into the the folder "reconstruction".
    2. Add initpath function to aspaire.

---
+++ initpath.m
@@ -0,0 +1,13 @@
+% Setup path
+
+fname = mfilename('fullpath');
+[pathstr,~,~] = fileparts(fname); % Find where the package installed.
+
+addpath(genpath(fullfile(pathstr,'abinitio')));
+addpath(genpath(fullfile(pathstr,'common')));
+addpath(genpath(fullfile(pathstr,'examples')));
+addpath(genpath(fullfile(pathstr,'fourier')));
+addpath(genpath(fullfile(pathstr,'projections')))
+addpath(genpath(fullfile(pathstr,'sinograms')))
+
+run(fullfile(pathstr,'reconstruction','FIRM','setup.m'));
