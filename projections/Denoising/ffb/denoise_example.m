a1cf0f26907c398674db040a39383e028684fe06
Tejal Bhamre <tbhamre@princeton.edu>
2016-07-18 16:02:05 -0400

    denoise example 1

--- projections/Denoising/ffb/denoise_example.m
+++ projections/Denoising/ffb/denoise_example.m
@@ -1,7 +1,8 @@
 %Denoise example code, input image
 var_hat = 1; %noise variance, change accordingly
 n_im = 3; % number of denoised images
-[ c, R ] = avg_pspec(data, var_hat); %Estimate band limit and compact support size
+energy_thresh=0.99;
+[ c, R ] = avg_pspec(data, var_hat, energy_thresh); %Estimate band limit and compact support size
 [ timing, coeff, mean_coeff, sPCA_coeff, U, D ] = jobscript_FFBsPCA(data, c, R, var_hat); % Compute Fast steerable PCA.
 [ fn ] = IFT_FB(R, c); % compute inverse fourier transform of the Fourier-Bessel functions
 L0=size(data,1);
