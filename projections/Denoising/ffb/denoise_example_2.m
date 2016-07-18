09fa6ea61f6bede1f6dd3ee183b6392159087c0f
Tejal Bhamre <tbhamre@princeton.edu>
2016-07-18 15:59:29 -0400

    denoise example

--- projections/Denoising/ffb/denoise_example_2.m
+++ projections/Denoising/ffb/denoise_example_2.m
@@ -2,7 +2,8 @@
 var_hat = 1; %noise variance, change accordingly
 n_im = 100; % number of denoised images
 data=ones(10,10,n_im)+0.1*randn(10,10,n_im);
-[ c, R ] = avg_pspec(data, var_hat); %Estimate band limit and compact support size
+energy_thresh=0.99;
+[ c, R ] = avg_pspec(data, var_hat, energy_thresh); %Estimate band limit and compact support size
 n_r = ceil(4*c*R);
 [ basis, sample_points ] = precomp_fb( n_r, R, c );
 
