6919364b01cf8ba39cfc181bbdf8c1c76e4a1791
Tejal Bhamre <tbhamre@princeton.edu>
2016-07-19 15:50:54 -0400

    Added num-cores

---
+++ common/num_cores.m
@@ -0,0 +1,26 @@
+ORES Count the number of cores available
+
+%
+
+% Usage
+
+%    n = num_cores();
+
+%
+% Output
+%    n: Number of cores available for parallel computing.
+
+function n = num_cores()
+	v = ver('distcomp');
+	if isempty(v.Version)
+		n = 1;
+	elseif str2double(v.Version) >= 6.3
+		p = gcp;
+		n = p.NumWorkers;
+	else
+		n = matlabpool('size');
+	end
+	n = max(1, n);
+end
+
+
