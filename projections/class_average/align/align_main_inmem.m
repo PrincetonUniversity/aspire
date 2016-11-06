f4976bea8be60edfd9d748e33f2e29b29c571022
Joakim Andén <janden@math.princeton.edu>
2016-11-05 22:31:27 -0400

    Add align_main_inmem wrapper as a workaround
    This will create a dummy structure and pass it to align_main,
    then read in the resulting class averages and provide them as
    output, mimicking the old behavior of align_main.

---
+++ projections/class_average/align/align_main_inmem.m
@@ -0,0 +1,43 @@
+function [shifts, corr, averages, norm_variance] = align_main_inmem( ...
+    images, angle, class_VDM, refl, FBsPCA_data, k, max_shifts, list_recon)
+
+% Function for aligning images with its k nearest neighbors to generate
+% class averages. Wrapper for the align_main function providing in-memory
+% functionality.
+%
+%   Usage:
+%       [shifts, corr, averages, norm_variance] = align_main_inmem(images, ...
+%          angle, class_VDM, refl, FBsPCA_data, k, max_shifts, list_recon);
+%   Input:
+%       images: LxLxP matrix. P projection images of size LxL pixels.
+%       angle: Pxl (l>=k) matrix. Rotational alignment angle
+%       class_VDM: Pxl matrix. Nearest neighbor list
+%       refl: Pxl matrix. 1: no reflection. 2: reflection
+%       FBsPCA_data: Fourier-Bessel steerable PCA data with r_max, UU,
+%       Coeff, Mean, Freqs
+%       k: number of nearest neighbors for class averages
+%       max_shifts: maximum number of pixels to check for shift
+%       list_recon: indices for images to compute class averages
+%   Output:
+%       shifts: Pxk matrix. Relative shifts for k nearest neighbors
+%       corr: Pxk matrix. Normalized cross correlation of each image with
+%       its k nearest neighbors
+%       average: LxLxP matrix. Class averages
+%       norm_variance: compute the variance of each class averages.
+%
+% Joakim Andén Nov 2016
+
+    data.dim = size(images);
+    data.getImage = @(s)(images(:,:,s));
+
+    tmpdir = tempname;
+    mkdir(tmpdir);
+
+    [shifts, corr, ave_filename, norm_variance] = align_main(data, angle, ...
+        class_VDM, refl, FBsPCA_data, k, max_shifts, list_recon, tmpdir);
+
+    averages = ReadMRC(ave_filename);
+
+    delete(ave_filename);
+    rmdir(tmpdir, 's');
+end
