818ef87c42e20f3f60a9611cc579ca165a4ca598
Yoel <yoelsh@post.tau.ac.il>
2016-12-27 23:25:01 +0200

    Create temporary files on /scratch

--- io/tempmrcname.m
+++ io/tempmrcname.m
@@ -4,7 +4,15 @@ function fname=tempmrcname
 %
 %   fname=tempmrcname
 %       Returns a temporary unique filename with extension '.mrc'
+%       Creates the temprary dir if needed.
 %
 % Yoel Shkolnisky, December 2016
 
-fname=[tempname '.mrc'];
+temporarydir='/scratch/aspire_temp';
+if ~exist(temporarydir,'dir')
+    log_message('Creating temporary dir %s',temporarydir);
+    mkdir(temporarydir);
+end
+
+[~,name]=fileparts(tempname);
+fname=fullfile(temporarydir,[name '.mrc']);
