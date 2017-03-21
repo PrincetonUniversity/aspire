fa19e09bf64a5f4d0887bca3e156c1384646770f
Joakim Andén <janden@math.princeton.edu>
2017-03-21 12:02:07 -0400

    Remove old NUFFT code
    Old MEX files and related scripts.

--- fourier/nufft/2d/makemex.m
+++
@@ -1,4 +0,0 @@
-mex -O nufft2doptimizedmx.cpp -I../common
-mex -O nufftt2dexecutemx.cpp -I../common
-mex -O nufftt2dnoqmx.cpp -I../common
-mex -O nufftt2dpreparemx.cpp -I../common
--- fourier/nufft/3d/makemex.m
+++
@@ -1,4 +0,0 @@
-mex -O nufft3dauxmx.cpp -I../common
-mex -O nufft3dauxmx_ref.cpp -I../common
-mex nufftt3dexecutemx_1.cpp ../common/printarr.cpp -I../common/
-mex nufftt3dexecutemx_2.cpp ../common/printarr.cpp -I../common/
