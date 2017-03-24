c99b314e637601bdf36907440376e1b4b9aac2f2
Yoel <yoelsh@post.tau.ac.il>
2017-03-24 23:11:00 +0300

    Revise cryo_plot_viewing_directions.m to plot theta and rho of only upper hemisphere.

--- io/visualization/cryo_plot_viewing_directions.m
+++ io/visualization/cryo_plot_viewing_directions.m
@@ -8,6 +8,8 @@ function [h1,h2]=cryo_plot_viewing_directions(rotations)
 %   "rotations" are the estimated rotations as returned, for example, by
 %   the synchronization algorithm. Returns a handle to the generated
 %   figure.
+%   Each point correspond to the (theta,phi) coordintaes of the view
+%   direction, colored by the polar angle phi (restricted to [0,90]).
 %
 % Yoel Shkolnisky, March 2015.
 
@@ -15,12 +17,26 @@ viewingdirs=rotations(:,3,:); % Viewing direction is the third column of
                               % each rotation matrix.
 viewingdirs=squeeze(viewingdirs);
 thetas=atan2d(viewingdirs(2,:),viewingdirs(1,:));
-%phis=acosd(viewingdirs(3,:));
-rhos=sqrt(1-viewingdirs(3,:).^2);
+phis=acosd(viewingdirs(3,:));
+%rhos=sqrt(1-viewingdirs(3,:).^2);
 
 h1=figure;
 %scatter(thetas(:),phis(:));
-polar(thetas/180*pi,rhos,'.');
+% The first parameters to polarscatter is indicating the angle in radians
+% of each point, and the second is indicating the radius for each point. We
+% use the polar angle as the radius.
+% Since projection by an angle great than phi > pi/2 is the same as
+% projection by projection by 180-phi plus in-plane rotation, we plot fold
+% all phis into the range [0,pi/2].
+
+folded_phis=phis;
+idx=find(folded_phis>90);
+folded_phis(idx)=180-folded_phis(idx);
+assert(all(folded_phis<=90))
+polarscatter(thetas/180*pi,folded_phis,8,folded_phis,'filled');
+rlim([0 90])
+rticks(0:10:90)
 
 h2=figure;
-scatter3(viewingdirs(1,:),viewingdirs(2,:),viewingdirs(3,:));
+scatter3(viewingdirs(1,:),viewingdirs(2,:),viewingdirs(3,:),5,folded_phis,'filled');
+axis equal
