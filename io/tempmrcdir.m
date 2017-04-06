a34347feed46207b4a355378e5ba927c8651d5b0
Yoel <yoelsh@post.tau.ac.il>
2017-04-06 20:18:09 +0300

    Append PID and optional key.

--- io/tempmrcdir.m
+++ io/tempmrcdir.m
@@ -1,15 +1,33 @@
-function dname=tempmrcdir
+function dname=tempmrcdir(key)
 %
 % TEMPMRCDIR Get teroprary directory
 %
 %   dname=tempmrcdir
 %       Returns a temporary directory name for ASPIRE output.
 %       Creates the directory if does not exist.
+%       The name of the directory is different for each running matlab
+%       process.
+%
+%   dname=tempmrcdir(key)
+%       Append key (string) to the directory name. This enables to generate
+%       two temporary directories for the same MATLAB process.
 %
 % Yoel Shkolnisky, January 2016
 
+
 % Look for a configuration file defining the location of the temporary
 % directory.
+
+if ~exist('key','var')
+    key=[];
+else
+    if ~ischar(key)
+        error('key should be string');
+    end
+end
+
+pid=feature('getpid');
+
 fname = mfilename('fullpath');
 [pathstr,~,~] = fileparts(fname); % Find where the package installed.
 
@@ -19,7 +37,7 @@ confname=fullfile(ASIPRE_ROOT,'tmpdir.cfg');
 
 tmpdir='scratch'; % Default root of temporary files
 if exist(confname,'file')
-    log_message('Loading name of temporary folder from %s',confname);
+    log_message('Loading name for root of temporary folders from %s',confname);
     fid=fopen(confname,'r');
     tmpdir=fscanf(fid,'%s');
     fclose(fid);
@@ -31,7 +49,13 @@ end
 
 
 uname=getenv('USER');
-dname=fullfile(filesep,tmpdir,uname,'aspire_temp');
+pidstr=int2str(pid);
+
+dname=fullfile(filesep,tmpdir,uname,'aspire_temp',pidstr);
+
+if ~isempty(key)
+    dname=fullfile(dname,key);
+end
 
 if ~exist(dname,'dir')
     log_message('Creating temporary dir %s',dname);
