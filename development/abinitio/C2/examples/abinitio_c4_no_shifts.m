fe59b50ffa027102f2faf6659ab82a05984f26c1
Gabi <gabipragier@gmail.com>
2017-05-11 13:36:23 +0300

    adding all C4 code as baseline

---
+++ development/abinitio/C2/examples/abinitio_c4_no_shifts.m
@@ -0,0 +1,86 @@
+tic;
+
+initstate; 
+open_log(0);
+
+%% Load and display projections
+% The MAT file p100_c4_shifted contains 100 projections of size 65x65. The
+% orientations (given as quaternions) used to generate these projections
+% are stored in the the variable "refq". The projection were generated using the following command:
+% [projs,refq] = generate_c4_images(100,100000000,65,'GAUSSIAN',0,1);
+
+load p100_c4_gaussian_no_shifts;
+viewstack(projs,5,5);   % Display the proejctions.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step 1  : Computing polar Fourier transform of projections
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+masked_projs = mask_fuzzy(projs,23);
+
+n_theta = 360; % number of rays in every projection
+n_r     = 89;  % number of radial points in every radial line
+[npf,~] = cryo_pft(masked_projs,n_r,n_theta,'single');
+
+npf = gaussian_filter_imgs(npf);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step 2  : detect a single pair of common-lines between each pair of images
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+max_shift  = 0; % number of shifts to consider
+shift_step = 0.5; % the shift step (see Yoel's technical report)
+clmatrix = cryo_clmatrix_gpu(npf,size(npf,3),1,max_shift,shift_step); 
+cl_detection_rate(clmatrix,n_theta,refq);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step 3  : detect self-common-lines in each image
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+is_handle_equator_ims = true;
+equator_res_fact = 10;
+equator_removal_frac = 0.1;
+sclmatrix = cryo_self_clmatrix_gpu(npf,max_shift,shift_step,...
+    is_handle_equator_ims,equator_res_fact,equator_removal_frac,refq);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step 4  : calculate self-relative-rotations
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Riis = estimate_all_Riis(sclmatrix,n_theta,refq);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step 5  : calculate relative-rotations
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Rijs = cryo_c4_estimate_all_Rijs(clmatrix,n_theta,refq);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step 6  : inner J-synchronization
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+is_remove_non_rank1 = true;
+non_rank1_remov_percent = 0.25;
+[vijs,viis,im_inds_to_remove,pairwise_inds_to_remove,...
+    npf,projs,refq] = local_sync_J(Rijs,Riis,npf,...
+                                projs,is_remove_non_rank1,non_rank1_remov_percent,refq);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step 7  : outer J-synchronization
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[vijs,viis] = global_sync_J(vijs,viis);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step 8  : third rows estimation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+vis  = estimate_third_rows(vijs,viis);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step 9  : in-plane rotations angles estimation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+inplane_rot_res = 1;
+[rots,in_plane_rotations] = estimate_inplane_rotations2(npf,vis,inplane_rot_res,max_shift,shift_step);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step 10  : Results Analysis
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[rot_alligned,err_in_degrees,mse] = analyze_results(rots,n_theta,refq);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step 11  : Reconstructing volume
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+estimatedVol = reconstruct(projs,rot_alligned,n_r,n_theta,max_shift,shift_step);   
+
+WriteMRC(estimatedVol,1,'example1_no_shifts.mrc');
---
+++ development/abinitio/C2/examples/cl_detection_rate.m
@@ -0,0 +1,115 @@
+function [detec_rate,clmatrix_correct] = cl_detection_rate(clmatrix,n_theta,refq)
+%
+% Checks the detection rate of common-lines between 
+% images of a c4 symmetric molecule which is invariant to handedness ambiguity. 
+% For each pair of images (i,j) the method checks if the
+% single pair of common line found is one of the four pairs of common lines
+% that are shared by the images.
+% 
+% Input parameters:
+%   clmatrix    A n-by-n table where n represens the number of images.  
+%               The (i,j) entry contains the index of a
+%               common-line in image i betwen images i and j. 
+%   n_theta     The angular resolution of common-lines. It is the number
+%               of lines in any given image. E.g., n_theta=360 means that
+%               each image has 360 lines
+%   refq        A 4-by-n table. The i-th column represent the quaternion of
+%               that corresponds to the rotation matrix of the i-th image
+%
+%
+% Output parameters:
+%   detec_rate        The detection rate (in [0,1]) of common-lines.
+%                      For example, detec_rate=1 means that for each pair
+%                      of images the common-line found is one of the four
+%                      common-lines between the corresponding images
+%
+%   clmatrix_correct  A boolean matrix of size n-by-n. the (i,j)-th entry
+%                     is equal 1 if the common line found is one of the 
+%                     four pairs of common lines between images i and j,
+%                     and is 0 otherwise. 
+
+angle_tol_err = 10/180*pi; % how much angular deviation we allow for a common-line to have
+nImages = size(clmatrix,1);
+clmatrix_correct = zeros(size(clmatrix));
+% clmatrix_gt is a n*n*4 matrix representing the four pairs of common-lines between each two images
+clmatrix_gt = find_cl_gt(n_theta,refq); 
+
+clmatrix_diff = bsxfun(@minus,clmatrix_gt,clmatrix);
+clmatrix_diff_angle = clmatrix_diff*2*pi./n_theta;
+% take absolute cosine because of handedness
+% there might be +180 independendt diff for each image which at this stage
+% hasn't been taken care yet.
+nCorrect = 0;
+hand_idx = zeros(1,nchoosek(nImages,2));
+for i=1:nImages
+    for j=i+1:nImages
+        ind = uppertri_ijtoind(i,j,nImages);
+        diffs_cij = clmatrix_diff_angle(i,j,:);
+        diffs_cji = clmatrix_diff_angle(j,i,:);
+        min_diff1 = min(acos(cos(diffs_cij))    + acos(cos(diffs_cji)));
+        min_diff2 = min(acos(cos(diffs_cij+pi)) + acos(cos(diffs_cji+pi)));
+        if min_diff1 < min_diff2
+            min_diff = min_diff1;
+            hand_idx(ind) = 1;
+        else
+            min_diff = min_diff2;
+            hand_idx(ind) = 2;
+        end
+        if min_diff < 2*angle_tol_err
+            nCorrect  = nCorrect+1;
+            clmatrix_correct(i,j) = 1;
+            clmatrix_correct(j,i) = 1;
+        end
+    end
+end
+
+cl_dist = histc(hand_idx,1:2)/numel(hand_idx);
+detec_rate = nCorrect/(nImages*(nImages-1)/2);
+log_message('common lines detection rate=%.2f%%',detec_rate*100);
+log_message('cl_J_dist=[%.2f %.2f]',cl_dist);
+
+end
+
+function clmatrix_gt = find_cl_gt(n_theta,refq)
+
+
+nImages = size(refq,2);
+clmatrix_gt = zeros(nImages,nImages,4);
+
+g = [0 -1 0; ...
+     1  0 0; ...
+     0  0 1]; % rotation matrix of 90 degress around z-axis
+
+gs = zeros(3,3,4);
+for s=0:3
+    gs(:,:,s+1) = g^s;
+end
+
+for i=1:nImages
+    for j=i+1:nImages
+        Ri = q_to_rot(refq(:,i))';
+        Rj = q_to_rot(refq(:,j))';
+        for s=0:3
+            U = Ri.'*gs(:,:,s+1)*Rj;
+            c1 = [-U(2,3)  U(1,3)]';
+            c2 = [ U(3,2) -U(3,1)]';
+            
+            idx1 = clAngles2Ind(c1,n_theta);
+            idx2 = clAngles2Ind(c2,n_theta);
+            
+%             if strcmp(params_simul.CL,'GT') && params_simul.confuse_cl_J
+%                 if round(rand)==1
+%                     % j-conjugating amounts at choosing the antipodal
+%                     % common-lines in each image
+%                     idx1 = mod(idx1+n_theta/2-1,n_theta)+1;
+%                     idx2 = mod(idx2+n_theta/2-1,n_theta)+1;
+%                 end
+%             end
+            clmatrix_gt(i,j,s+1) = idx1;
+            clmatrix_gt(j,i,s+1) = idx2;
+        end
+    end
+end
+
+
+end
