001b3cd995792052c771d969dc9e26df124d5ad3
Joakim Andén <janden@math.princeton.edu>
2017-06-19 21:35:43 -0400

    Add Fourier full<->half functions
    Used if we want to deal with the postiive half-space of Fourier
    transforms.

---
+++ fourier/fourier_flip.m
@@ -0,0 +1,32 @@
+% FOURIER_FLIP Flip Fourier transform along specified dimensions
+%
+% Usage
+%    yf = fourier_flip(xf, dims);
+%
+% Input
+%    xf: The Fourier transform to be flipped.
+%    dims: A vector of dimensions along which to flip.
+%
+% Output
+%    yf: The Fourier transform xf, but flipped along the specified dimensions
+%       with the zero frequency kept intact.
+%
+% Notes
+%    For the moment, only even dimensions are supported.
+
+function xf = fourier_flip(xf, dims)
+    idx_src.type = '()';
+    idx_dst.type = '()';
+
+    colon = {':'};
+
+    idx_dst.subs = colon(ones(1, ndims(xf)));
+    idx_src.subs = colon(ones(1, ndims(xf)));
+
+    for dim = dims
+        idx_dst.subs{dim} = 1:size(xf, dim);
+        idx_src.subs{dim} = [1 size(xf, dim):-1:2];
+    end
+
+    xf = subsasgn(xf, idx_dst, subsref(xf, idx_src));
+end
