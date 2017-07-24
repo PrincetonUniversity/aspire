fba33659f000c967f93e555792b1b9ffe979863b
Joakim Andén <janden@math.princeton.edu>
2017-07-24 15:48:53 -0400

    Allow multiple noise responses in `cryo_prewhiten`
    So that if we have multiple noise responses (one for each image)
    we can use these to whiten the images simultaneously.

--- projections/epsd/cryo_prewhiten.m
+++ projections/epsd/cryo_prewhiten.m
@@ -3,7 +3,11 @@ function [ proj, filter, nzidx] = cryo_prewhiten(proj, noise_response )
 %  
 %  Input:
 %   proj            Stack of images.
-%   noise_response  2d image with the power spectrum of the noise.
+%   noise_response  2d image with the power spectrum of the noise. If all
+%      images are to be whitened with respect to the same power spectrum,
+%      this is a single image. If each image is to be whitened with respect
+%      to a different power spectrum, this is a three-dimensional array with
+%      the same number of 2d slices as the stack of images.
 %
 %  Output:
 %   proj    Pre-whitened stack of images.
@@ -21,6 +25,10 @@ n=size(proj, 3);
 L=size(proj, 1); 
 K=size(noise_response, 1);
 
+if size(noise_response, 3) ~= n && size(noise_response, 3) ~= 1
+    error('The number of filters must be either 1 or n.');
+end
+
 % The whitening filter is the sqrt of of the power spectrum of the noise.
 % Also, normalized the enetgy of the filter to one.
 filter=sqrt(noise_response);      
@@ -28,15 +36,16 @@ filter=sqrt(noise_response);
 % The power spectrum of the noise must be positive, and then, the values
 % in filter are all real. If they are not, this means that noise_response
 % had negative values so abort.
-assert(norm(imag(filter(:)))<10*delta); % Allow loosing one digit.
+assert(norm(imag(filter(:)))<10*delta*size(filter, 3)); % Allow loosing one digit.
 filter=real(filter);  % Get rid of tiny imaginary components, if any.
 
-% The filter should be cicularly symmetric. In particular, should have
+% The filter should be cicularly symmetric. In particular, it should have
 % reflection symmetry.
-assert(norm(filter-fourier_flip(filter, [1 2]))<10*delta); 
+filter_flipped = fourier_flip(filter, [1 2]);
+assert(norm(filter(:)-filter_flipped(:))<10*delta*size(filter, 3)); 
 
 % Get rid of any tiny asymmetries in the filter.
-filter = 0.5*(filter + fourier_flip(filter, [1 2]));
+filter = 0.5*(filter + filter_flipped);
 
 % The filter may have very small values or even zeros. We don't want to
 % process these so make a list of all large entries.
@@ -46,20 +55,30 @@ nzidx = nzidx(:);
 
 fnz=filter(nzidx);
 
-% TODO: Batch this to avoid memory trouble with large n if the noise_response
-% is much larger than the projections.
+if size(filter, 3) == 1
+    % TODO: Batch this to avoid memory trouble with large n if the noise_response
+    % is much larger than the projections.
 
-pp = pad_signal(proj, K*ones(1, 2));
-fp=cfft2(pp); % Take the Fourier transform of the padded image.
-fp = reshape(fp, [L^2 n]);
-p=zeros([L^2 n]);
-% Divide the image by the whitening filter, 
-% but onlyin places where the filter is
-% large. In frequnecies where the filter is
-% tiny  we cannot pre-whiten so we just put
-% zero.
-p(nzidx,:) = bsxfun(@times, fp(nzidx,:), 1./fnz);
-p = reshape(p, [L*ones(1, 2) n]);
+    pp = pad_signal(proj, K*ones(1, 2));
+    fp=cfft2(pp); % Take the Fourier transform of the padded image.
+    fp = reshape(fp, [L^2 n]);
+    p=zeros([L^2 n]);
+    % Divide the image by the whitening filter, 
+    % but onlyin places where the filter is
+    % large. In frequnecies where the filter is
+    % tiny  we cannot pre-whiten so we just put
+    % zero.
+    p(nzidx,:) = bsxfun(@times, fp(nzidx,:), 1./fnz);
+    p = reshape(p, [L*ones(1, 2) n]);
+else
+    pp = pad_signal(proj, K*ones(1, 2));
+
+    fp = cfft2(pp);
+
+    p = zeros([L*ones(1, 2) n]);
+
+    p(nzidx) = fp(nzidx)./fnz;
+end
 p2 = icfft2(p);
 assert(norm(imag(p2(:)))/norm(p2(:))<1.0e-13); % The resulting image should be real.
 p2 = unpad_signal(p2, L*ones(1, 2));
