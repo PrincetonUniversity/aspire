e42b6ca3dc9f95930224649520e3b540dd7c523e
Joakim Andén <janden@math.princeton.edu>
2017-07-26 17:58:56 -0400

    Clean up `initpath`
    Make it into a function, remove unused paths, etc.

--- initpath.m
+++ initpath.m
@@ -1,32 +1,34 @@
-% Setup path
+% INITPATH Initialize paths for ASPIRE toolbox
+%
+% Usage
+%    initpath();
 
-fname = mfilename('fullpath');
-[pathstr,~,~] = fileparts(fname); % Find where the package installed.
+function initpath()
+    fname = mfilename('fullpath');
+    [pathstr,~,~] = fileparts(fname); % Find where the package installed.
 
-addpath(genpath(fullfile(pathstr,'abinitio')));
-addpath(genpath(fullfile(pathstr,'common')));
-addpath(genpath(fullfile(pathstr,'examples')));
-addpath(genpath(fullfile(pathstr,'fourier')));
-addpath(genpath(fullfile(pathstr,'io')))
-addpath(genpath(fullfile(pathstr,'projections')))
-addpath(genpath(fullfile(pathstr,'sinograms')))
-addpath(genpath(fullfile(pathstr,'reconstruction')))
-addpath(genpath(fullfile(pathstr,'refinement')))
-addpath(genpath(fullfile(pathstr,'workflow')))
+    addpath(genpath(fullfile(pathstr,'abinitio')));
+    addpath(genpath(fullfile(pathstr,'common')));
+    addpath(genpath(fullfile(pathstr,'examples')));
+    addpath(genpath(fullfile(pathstr,'fourier')));
+    addpath(genpath(fullfile(pathstr,'io')))
+    addpath(genpath(fullfile(pathstr,'projections')))
+    addpath(genpath(fullfile(pathstr,'sinograms')))
+    addpath(genpath(fullfile(pathstr,'reconstruction')))
+    addpath(genpath(fullfile(pathstr,'refinement')))
+    addpath(genpath(fullfile(pathstr,'workflow')))
 
-%run(fullfile(pathstr,'extern','irt','setup.m'))
-%run(fullfile(pathstr,'extern','cvx','cvx_startup.m'));
+    if exist(fullfile(pathstr, 'extern', 'nufftall-1.33'))
+        addpath(fullfile(pathstr, 'extern', 'nufftall-1.33'));
+    end
 
-if exist(fullfile(pathstr, 'extern', 'nufftall-1.33'))
-    addpath(fullfile(pathstr, 'extern', 'nufftall-1.33'));
-end
+    if exist(fullfile(pathstr, 'extern', 'nfft'))
+        addpath(fullfile(pathstr, 'extern', 'nfft', 'lib'));
+        addpath(fullfile(pathstr, 'extern', 'nfft', 'share', 'nfft', ...
+            'matlab', 'nfft'));
+    end
 
-if exist(fullfile(pathstr, 'extern', 'nfft'))
-    addpath(fullfile(pathstr, 'extern', 'nfft', 'lib'));
-    addpath(fullfile(pathstr, 'extern', 'nfft', 'share', 'nfft', ...
-        'matlab', 'nfft'));
-end
-
-if exist(fullfile(pathstr, 'extern', 'SDPLR-1.03-beta'))
-    addpath(fullfile(pathstr,'extern','SDPLR-1.03-beta'));
+    if exist(fullfile(pathstr, 'extern', 'SDPLR-1.03-beta'))
+        addpath(fullfile(pathstr,'extern','SDPLR-1.03-beta'));
+    end
 end
