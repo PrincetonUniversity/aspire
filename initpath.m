5848b668f34a5422b36dc7649ca895d4e3901513
Joakim Andén <janden@math.princeton.edu>
2017-08-08 20:26:39 -0400

    Make `initpath` a script again
    Turns out `mfilename` doesn't work if it's a function, so we need
    it as a script. In addition, we can't use `run` on it if it's a
    function.

--- initpath.m
+++ initpath.m
@@ -3,35 +3,35 @@
 % Usage
 %    initpath();
 
-function initpath()
-    fname = mfilename('fullpath');
-    [pathstr,~,~] = fileparts(fname); % Find where the package installed.
+% Find where the package installed.
+[__pathstr, ~, ~] = fileparts(mfilename('fullpath'));
 
-    addpath(pathstr);
+addpath(__pathstr);
 
-    addpath(genpath(fullfile(pathstr,'abinitio')));
-    addpath(genpath(fullfile(pathstr,'basis')));
-    addpath(genpath(fullfile(pathstr,'common')));
-    addpath(genpath(fullfile(pathstr,'examples')));
-    addpath(genpath(fullfile(pathstr,'fourier')));
-    addpath(genpath(fullfile(pathstr,'io')))
-    addpath(genpath(fullfile(pathstr,'projections')))
-    addpath(genpath(fullfile(pathstr,'sinograms')))
-    addpath(genpath(fullfile(pathstr,'reconstruction')))
-    addpath(genpath(fullfile(pathstr,'refinement')))
-    addpath(genpath(fullfile(pathstr,'workflow')))
+addpath(genpath(fullfile(__pathstr,'abinitio')));
+addpath(genpath(fullfile(__pathstr,'basis')));
+addpath(genpath(fullfile(__pathstr,'common')));
+addpath(genpath(fullfile(__pathstr,'examples')));
+addpath(genpath(fullfile(__pathstr,'fourier')));
+addpath(genpath(fullfile(__pathstr,'io')))
+addpath(genpath(fullfile(__pathstr,'projections')))
+addpath(genpath(fullfile(__pathstr,'sinograms')))
+addpath(genpath(fullfile(__pathstr,'reconstruction')))
+addpath(genpath(fullfile(__pathstr,'refinement')))
+addpath(genpath(fullfile(__pathstr,'workflow')))
 
-    if exist(fullfile(pathstr, 'extern', 'nufftall-1.33'))
-        addpath(fullfile(pathstr, 'extern', 'nufftall-1.33'));
-    end
-
-    if exist(fullfile(pathstr, 'extern', 'nfft'))
-        addpath(fullfile(pathstr, 'extern', 'nfft', 'lib'));
-        addpath(fullfile(pathstr, 'extern', 'nfft', 'share', 'nfft', ...
-            'matlab', 'nfft'));
-    end
-
-    if exist(fullfile(pathstr, 'extern', 'SDPLR-1.03-beta'))
-        addpath(fullfile(pathstr,'extern','SDPLR-1.03-beta'));
-    end
+if exist(fullfile(__pathstr, 'extern', 'nufftall-1.33'))
+    addpath(fullfile(__pathstr, 'extern', 'nufftall-1.33'));
 end
+
+if exist(fullfile(__pathstr, 'extern', 'nfft'))
+    addpath(fullfile(__pathstr, 'extern', 'nfft', 'lib'));
+    addpath(fullfile(__pathstr, 'extern', 'nfft', 'share', 'nfft', ...
+        'matlab', 'nfft'));
+end
+
+if exist(fullfile(__pathstr, 'extern', 'SDPLR-1.03-beta'))
+    addpath(fullfile(__pathstr,'extern','SDPLR-1.03-beta'));
+end
+
+clear __pathstr;
