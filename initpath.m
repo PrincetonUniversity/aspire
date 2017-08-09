730642593e86f274715e4c8bc42426d201117514
Joakim Andén <janden@math.princeton.edu>
2017-08-08 21:24:08 -0400

    Fix `initpath` script
    Cannot begin variable names with "_" in MATLAB.

--- initpath.m
+++ initpath.m
@@ -4,34 +4,34 @@
 %    initpath();
 
 % Find where the package installed.
-[__pathstr, ~, ~] = fileparts(mfilename('fullpath'));
+[pathstr, ~, ~] = fileparts(mfilename('fullpath'));
 
-addpath(__pathstr);
+addpath(pathstr);
 
-addpath(genpath(fullfile(__pathstr,'abinitio')));
-addpath(genpath(fullfile(__pathstr,'basis')));
-addpath(genpath(fullfile(__pathstr,'common')));
-addpath(genpath(fullfile(__pathstr,'examples')));
-addpath(genpath(fullfile(__pathstr,'fourier')));
-addpath(genpath(fullfile(__pathstr,'io')))
-addpath(genpath(fullfile(__pathstr,'projections')))
-addpath(genpath(fullfile(__pathstr,'sinograms')))
-addpath(genpath(fullfile(__pathstr,'reconstruction')))
-addpath(genpath(fullfile(__pathstr,'refinement')))
-addpath(genpath(fullfile(__pathstr,'workflow')))
+addpath(genpath(fullfile(pathstr,'abinitio')));
+addpath(genpath(fullfile(pathstr,'basis')));
+addpath(genpath(fullfile(pathstr,'common')));
+addpath(genpath(fullfile(pathstr,'examples')));
+addpath(genpath(fullfile(pathstr,'fourier')));
+addpath(genpath(fullfile(pathstr,'io')))
+addpath(genpath(fullfile(pathstr,'projections')))
+addpath(genpath(fullfile(pathstr,'sinograms')))
+addpath(genpath(fullfile(pathstr,'reconstruction')))
+addpath(genpath(fullfile(pathstr,'refinement')))
+addpath(genpath(fullfile(pathstr,'workflow')))
 
-if exist(fullfile(__pathstr, 'extern', 'nufftall-1.33'))
-    addpath(fullfile(__pathstr, 'extern', 'nufftall-1.33'));
+if exist(fullfile(pathstr, 'extern', 'nufftall-1.33'))
+    addpath(fullfile(pathstr, 'extern', 'nufftall-1.33'));
 end
 
-if exist(fullfile(__pathstr, 'extern', 'nfft'))
-    addpath(fullfile(__pathstr, 'extern', 'nfft', 'lib'));
-    addpath(fullfile(__pathstr, 'extern', 'nfft', 'share', 'nfft', ...
+if exist(fullfile(pathstr, 'extern', 'nfft'))
+    addpath(fullfile(pathstr, 'extern', 'nfft', 'lib'));
+    addpath(fullfile(pathstr, 'extern', 'nfft', 'share', 'nfft', ...
         'matlab', 'nfft'));
 end
 
-if exist(fullfile(__pathstr, 'extern', 'SDPLR-1.03-beta'))
-    addpath(fullfile(__pathstr,'extern','SDPLR-1.03-beta'));
+if exist(fullfile(pathstr, 'extern', 'SDPLR-1.03-beta'))
+    addpath(fullfile(pathstr,'extern','SDPLR-1.03-beta'));
 end
 
-clear __pathstr;
+clear pathstr;
