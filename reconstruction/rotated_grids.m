fe18de1c9f9457bfd4330c18f6471cee6a53894d
Joakim Andén <janden@flatironinstitute.org>
2017-10-19 17:32:33 -0500

    Allow half-pixel shift in `rotated_grids`

--- reconstruction/rotated_grids.m
+++ reconstruction/rotated_grids.m
@@ -1,18 +1,28 @@
 % ROTATED_GRIDS Generate rotated Fourier grids in 3D from rotation matrices
 %
 % Usage
-%    pts_rot = rotated_grids(N, rot_matrices);
+%    pts_rot = rotated_grids(N, rot_matrices, half_pixel);
 %
 % Input
 %    N: The resolution of the desired grids.
 %    rot_matrices: An array of size 3-by-3-by-K containing K rotation matrices.
+%    half_pixel: If true, centers the rotation around a half-pixel (default false).
 %
 % Output
 %    pts_rot: A set of rotated Fourier grids in three dimensions as specified by
 %       the rotation matrices. Frequencies are in the range [-pi, pi].
 
-function pts_rot = rotated_grids(L, rot_matrices)
+function pts_rot = rotated_grids(L, rot_matrices, half_pixel)
+    if nargin < 3 || isempty(half_pixel)
+        half_pixel = false;
+    end
+
     mesh2d = mesh_2d(L);
+
+    if half_pixel
+        mesh2d.x = mesh2d.x + 1/L;
+        mesh2d.y = mesh2d.y + 1/L;
+    end
 
     num_pts = L^2;
     num_rots = size(rot_matrices, 3);
