5267833668fa1bd9f8032cfb73f7614141875655
Joakim Andén <janden@flatironinstitute.org>
2017-10-20 06:21:14 -0500

    Use `epsilon` field for NFFT

--- fourier/nufft/nufft_initialize.m
+++ fourier/nufft/nufft_initialize.m
@@ -30,14 +30,30 @@ function plan = nufft_initialize(sz, num_pts)
 	plan.epsilon = 1e-15;
 
 	if lib_code == 3
+		m = epsilon_to_nfft_cutoff(plan.epsilon);
+
+		nfft_flags = PRE_PHI_HUT | PRE_PSI | FFT_OUT_OF_PLACE;
+		fftw_flags = FFTW_ESTIMATE;
+
 		if numel(sz) == 1
-			plan.nfft_plan_id = nfft_init_1d(sz(1), num_pts);
+			plan.nfft_plan_id = nfft_init_guru(1, sz(1), ...
+				num_pts, 2*sz(1), m, nfft_flags, fftw_flags);
 		elseif numel(sz) == 2
-			plan.nfft_plan_id = nfft_init_2d(sz(1), sz(2), num_pts);
+			plan.nfft_plan_id = nfft_init_guru(2, sz(1), sz(2), ...
+				num_pts, 2*sz(1), 2*sz(2), m, nfft_flags, fftw_flags);
 		elseif numel(sz) == 3
-			plan.nfft_plan_id = nfft_init_3d(sz(1), sz(2), sz(3), num_pts);
+			plan.nfft_plan_id = nfft_init_guru(3, sz(1), sz(2), sz(3), ...
+				num_pts, 2*sz(1), 2*sz(2), 2*sz(3), m, nfft_flags, fftw_flags);
 		end
 	elseif ~ismember(lib_code, [1 2 4])
 		error('Invalid library code.');
 	end
+end
+
+function m = epsilon_to_nfft_cutoff(epsilon)
+	% NOTE: These are obtained empirically. Should have a theoretical
+	% derivation.
+	rel_errs = [6e-2 2e-3 2e-5 2e-7 3e-9 4e-11 4e-13 0];
+
+	m = find(epsilon > rel_errs, 1);
 end
