7a4ec13bb86601f272d2ade01de08ea29376d1a7
Yoel <yoelsh@post.tau.ac.il>
2018-01-05 21:03:01 +0200

    Make mergeSTAR faster.

--- io/star/mergeSTAR.m
+++ io/star/mergeSTAR.m
@@ -1,4 +1,4 @@
-function mergeSTAR(outstar,inpattern,verbose)
+function mergeSTAR(outstar,inpattern)
 % MERGESTAR  Combine STAR files
 % 
 % mergeSTAR(outstar,'instar*.mrc')
@@ -13,10 +13,6 @@ function mergeSTAR(outstar,inpattern,verbose)
 %
 % Yoel Shkolnisky, December 2017.
 
-if ~exist('verbose','var')
-    verbose=0;
-end
-
 files=dir(inpattern);
 nfiles=numel(files);
 
@@ -28,46 +24,15 @@ end
 fname=fullfile(files(1).folder,files(1).name);
 log_message('Reading %s to read field names',fname);
 datablock=readSTAR(fname,0);
-
-% Create a data structure with the field names into which we will insert
-% the data of each record of each of the input STAR files
-recfields=fieldnames(datablock.data{1});
-newdatarec=cell(numel(recfields)*2,1);
-for k=1:numel(recfields)
-    newdatarec{2*k-1}=recfields(k);
-end
-
-nrecs=0;
-% First pass - count number of records (for speedup of later updates)
-log_message('First pass - counting records');
-for idx=1:nfiles
-    fname=fullfile(files(idx).folder,files(idx).name);
-    log_message('Reading file %d/%d (%s)',idx,nfiles,fname);
-    datablock=readSTAR(fname,0);
-    nrecs=nrecs+numel(datablock.data);
-end    
-
-outdata=createSTARdata(nrecs,datablock.labels{:});
-
+outdata=createSTARdata(0,datablock.labels{:});
 
 % Read data from all files
-log_message('Second pass - reading data');
-currentrec=1;
 for idx=1:nfiles
     fname=fullfile(files(idx).folder,files(idx).name);
     log_message('Reading file %d/%d (%s)',idx,nfiles,fname);
     datablock=readSTAR(fname,0);
     
-    % Populate values into newdatarec from all available files
-    for recidx=1:numel(datablock.data)
-        currentdatarec=datablock.data{recidx};
-        for k=1:numel(recfields)
-            newdatarec{2*k}=currentdatarec.(recfields{k});
-        end
-        outdata=addrecordtoSTARdata(outdata,currentrec,newdatarec{:});
-        currentrec=currentrec+1;
-    end
-
+    outdata.data=vertcat(outdata.data(:),datablock.data(:));
 end    
 log_message('Saving %s',outstar);
 writeSTAR(outdata,outstar);
