96425e43ec1286366803477a5e902a4ac6a623e5
Joakim Andén <janden@flatironinstitute.org>
2018-02-13 16:08:36 -0500

    Add CTFs to `compare_firm`

--- development/reconstruction/compare_firm.m
+++ development/reconstruction/compare_firm.m
@@ -3,8 +3,20 @@ K = 1000;
 SNR = 1000;
 max_shift = 0.1*n;
 shift_step = max_shift/10;
+defocus = linspace(1000, 2500, 7);
 [projections, ~, shifts, rotations] = cryo_gen_projections(n, K, SNR, ...
     max_shift, shift_step);
+
+ctf = zeros([n*ones(1, 2) numel(defocus)]);
+ctf_idx = mod(0:K-1, numel(defocus))+1;
+
+for k = 1:numel(defocus)
+    ctf(:,:,k) = cryo_CTF_Relion(n, 300, defocus(k), defocus(k), 0, 2.0, ...
+        1.4, 0.1);
+
+    projections(:,:,ctf_idx == k) = ...
+        cryo_addctf(projections(:,:,ctf_idx == k), ctf(:,:,k));
+end
 
 tol = 1e-6;
 max_iter = 1000;
@@ -13,7 +25,8 @@ inv_rotations = invert_rots(rotations);
 
 fprintf('Reconstruction from clean projections\n');
 tic;
-v1 = recon3d_firm(projections, inv_rotations, -shifts, tol, max_iter, []);
+v1 = recon3d_firm_ctf(projections, ctf, ctf_idx, inv_rotations, -shifts, ...
+    tol, max_iter, []);
 t1 = toc;
 
 f = load(fullfile('projections', 'simulation', 'maps', 'cleanrib.mat'));
@@ -25,8 +38,8 @@ fprintf('The relative error of reconstruction is %f.\n',...
 
 params = struct();
 params.rot_matrices = inv_rotations;
-params.ctf = ones(size(projections, 1)*ones(1, 2));
-params.ctf_idx = ones(size(projections, 3), 1);
+params.ctf = ctf;
+params.ctf_idx = ctf_idx;
 params.shifts = shifts.';
 params.ampl = ones(size(projections, 3), 1);
 
