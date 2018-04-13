40b6cffa3b9fbbff3350e6a4fa3da98dd0eec96f
Joakim Andén <janden@flatironinstitute.org>
2018-04-13 12:25:56 -0400

    Add more documentation for `Bessel_ns_radial`

--- projections/Denoising/ffb/Bessel_ns_radial.m
+++ projections/Denoising/ffb/Bessel_ns_radial.m
@@ -14,6 +14,23 @@
 %           - ang_freqs: angular frequencies,
 %           - rad_freqs: radial frequencies, and
 %           - n_theta: number of samples on concentric rings.
+%
+% Description
+%    The radial basis functions computed are of the form
+%
+%       \phi_{k, q}(u) = c_{k, q} J_k ( R_{k, q} u / c ),
+%
+%    where u is the radial frequency in [0, c], J_k is the kth Bessel
+%    function, R_{k, q} is its qth zero, and c_{k, q} is a normalization
+%    constant so that
+%
+%       \int_0^c | \phi_{k, q} (u) |^2 u du = 1/(2 pi).
+%
+%    Its values are sampled at the points given by r.
+%
+%    The basis.Phi_ns cell array contains these sampled radial basis functions
+%    arranged according to the angular frequency k. Each element is a matrix
+%    whose columns correspond to the radial frequency q.
 
 % Written by Zhizhen Zhao - 11/25/2014
 % Reformated and documented by Joakim Anden - 2018-Apr-13
@@ -25,6 +42,11 @@ function basis = Bessel_ns_radial(c, R, r)
         'bessel.mat'));
     B = f.bessel(f.bessel(:, 4)<= 2*pi*c*R, :);
     clear f;
+
+    % First column of B contains Bessel order k
+    % Second column contains zero index q
+    % Third column contains location of qth zero R_{k, q} of J_k(u)
+    % Fourth column contains R_{k, q+1}
 
     ang_freqs = B(:, 1);
     max_ang_freqs = max(ang_freqs);
