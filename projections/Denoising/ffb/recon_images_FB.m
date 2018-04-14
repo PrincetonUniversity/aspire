61dc386f5b99b17f9eb880fbe908583230ebe20f
Joakim Andén <janden@flatironinstitute.org>
2018-04-13 21:07:51 -0400

    Reformat and document `recon_images_FB`

--- projections/Denoising/ffb/recon_images_FB.m
+++ projections/Denoising/ffb/recon_images_FB.m
@@ -1,45 +1,47 @@
-function [recon] = recon_images_FB(c, R, L0, Coeff, n_min, n_max)
-% Description
-% This function reconstruct image in real space from Fourier-Bessel expansion coefficients
-% Input:
-%	c: band limit
-%	R: compact support radius R
-%	L0: original size of the images
-%	Coeff: Fourier-Bessel expansion coefficients in cell structure
-  %     n_min: Starting image
-%	n_max: End image
-% Output:
-%	recon: reconstructed images in real domain
-% Update: 10/15 Zhizhen Zhao 
+% RECON_IMAGES_FB Reconstruct images from Fourier-Bessel coefficients
+%
+% Usage
+%    recon = recon_images_FB(c, R, L0, Coeff, n_min, n_max);
+%
+% Input
+%    c: The band limit of the basis in frequency.
+%    R: The radius of the disk on which the basis is concentrated.
+%    L0: The size of the images.
+%    Coeff: The Fourier-Bessel coefficients in the same format as FBcoeff_nfft.
+%    n_min, n_max: The range of images indices to reconstruct.
+%
+% Output
+%    recon: An array of size L0-by-L0-by-(n_max-n_min+1) containing the
+%       reconstructed images.
 
-%provide bandlimit c and compact support size R
-%provide expansion coefficients 'Coeff'
-L = 2*R;
-%Computes eigen images, need output from IFT_FB.m.
-[ fn ] = IFT_FB(R, c);
-max_ang_freqs = size(fn, 1)-1; %find k_max
-origin = floor(L0/2) + 1;
+% Written by Zhizhen Zhao - 10/15
+% Reformatted and documented by Joakim Anden - 2018-Apr-13
 
-tmp1 = fn{1};
-tmp1 = reshape(tmp1, L^2, size(tmp1, 3));
+function recon = recon_images_FB(c, R, L0, Coeff, n_min, n_max)
+    L = 2*R;
 
-recon = zeros(L0, L0, n_max-n_min+1);
-for K = n_min:n_max
-    tmp2 = tmp1*Coeff{1}(:, K);
-    tmp2 = reshape(tmp2, L, L);
-    I = real(tmp2);
-    for k = 1:max_ang_freqs
-        tmp = fn{k+1};
-        tmp = reshape(tmp, L^2, size(tmp, 3));
-        tmp2_pos = tmp*Coeff{k+1}(:, K);
-        tmp_2 = 2*real(tmp2_pos);
-        I = I + reshape(tmp_2, L, L);
-    end;
+    fn = IFT_FB(R, c);
+    max_ang_freqs = size(fn, 1)-1;
+    origin = floor(L0/2)+1;
 
-    test = zeros(L0);
-    test( origin-R : origin+R - 1, origin-R : origin+R-1) = real(I);
-    recon(:, :, K-n_min+1) = test;
-%    sprintf('Done reconstructing %dth image',K)
+    tmp1 = fn{1};
+    tmp1 = reshape(tmp1, L^2, size(tmp1, 3));
 
-end;
+    recon = zeros(L0, L0, n_max-n_min+1);
+    for K = n_min:n_max
+        tmp2 = tmp1*Coeff{1}(:,K);
+        tmp2 = reshape(tmp2, L, L);
+        I = real(tmp2);
+        for k = 1:max_ang_freqs
+            tmp = fn{k+1};
+            tmp = reshape(tmp, L^2, size(tmp, 3));
+            tmp2_pos = tmp*Coeff{k+1}(:,K);
+            tmp_2 = 2*real(tmp2_pos);
+            I = I + reshape(tmp_2, L, L);
+        end
 
+        test = zeros(L0);
+        test(origin-R:origin+R-1,origin-R:origin+R-1) = real(I);
+        recon(:,:,K-n_min+1) = test;
+    end
+end
