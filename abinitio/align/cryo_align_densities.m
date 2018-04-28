fd749b9f8c8970dc23a272f35d1bac110002a9e7
Yoel Shkolnisky <yoelsh@post.tau.ac.il>
2018-04-28 22:52:01 +0300

    Minor fixes.
    cryo_abinitio_C1_worker.m	Fix variable name
    cryo_align_densities.m 		Add cutoff parameter
    cryo_compare_stacks.m		Detect images with NaNs
    cryo_workflow_classify.m 	Increase VDM_out to 500

--- abinitio/align/cryo_align_densities.m
+++ abinitio/align/cryo_align_densities.m
@@ -1,4 +1,4 @@
-function [estR,estdx,vol2aligned,reflect]=cryo_align_densities_old(vol1,vol2,pixA,verbose,Rref,forcereflect)
+function [estR,estdx,vol2aligned,reflect]=cryo_align_densities(vol1,vol2,pixA,verbose,cutoff,Rref,forcereflect)
 % 
 % Deprecated function.
 % Ths function is based on brute-force search of the aligment parameters.
@@ -21,10 +21,14 @@ function [estR,estdx,vol2aligned,reflect]=cryo_align_densities_old(vol1,vol2,pix
 %       Use pixel size in Angstrom pixA to compute the resolution.
 %       Set to non-positive number to ignore.
 
-% [Rest,estdx,vol2aligned,reflect]=cryo_align_densities(vol1,vol2,pixA)
+% [Rest,estdx,vol2aligned,reflect]=cryo_align_densities(vol1,vol2,pixA,verbose)
 %       Set verbose to nonzero for verbose printouts. 
-
-% [Rest,estdx,vol2aligned,reflect]=cryo_align_densities(vol1,vol2,verbose,Rref)
+%
+% [Rest,estdx,vol2aligned,reflect]=cryo_align_densities(vol1,vol2,verbose,cutoff)
+%       Use given FSC cutoff value. cutoff is the FSC threshold to use
+%       for reporting resolutions. Default is 0.143.
+%
+% [Rest,estdx,vol2aligned,reflect]=cryo_align_densities(vol1,vol2,verbose,cutoff,Rref)
 %       If the true rotation between vol1 and vol2 in known (during
 %       development/debugging), the function uses Rref to provide detailed
 %       debugging messages. Rref is ignored if reflection is detected.
@@ -39,6 +43,10 @@ end
 
 if ~exist('verbose','var')
     verbose=0;
+end
+
+if ~exist('cutoff','var')
+    cutoff=0.143;
 end
 
 %% Validate input
@@ -252,7 +260,7 @@ estR=bestR.';
 estdx=bestdx;
 
 fsc=FSCorr(vol1,vol2aligned);
-bestRes=fscres(fsc,0.143);
+bestRes=fscres(fsc,cutoff);
 bestResA=2*pixA*numel(fsc)/bestRes; % Resolution in Angstrom.
 
 
