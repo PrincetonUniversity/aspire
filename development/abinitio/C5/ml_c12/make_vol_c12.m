59f57821dad4806ad639345df95ba3c53c141873
Gabi <gabipragier@gmail.com>
2018-05-02 22:46:32 +0300

    removing redundant ml_c4_old and ml_c12 folders

--- development/abinitio/C5/ml_c12/make_vol_c12.m
+++
@@ -1,9 +0,0 @@
-function vol_out = make_vol_c12(vol_in)
-
-vol_out = vol_in;
-for i=1:11
-    vol_out = vol_out + fastrotate3z(vol_in,i*360/12);
-end
-vol_out = vol_out/12;
-
-end
--- development/abinitio/C5/ml_c4_old/initpath.m
+++
@@ -1,4 +0,0 @@
-run('/home/gabip/matlabProjects/aspire/aspire/initpath_development.m');
-run('../../../../install_cims_nufft')
-
-
