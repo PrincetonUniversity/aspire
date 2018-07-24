f0af636a1eb7a5fa4df02dffbc4a63df7d10b499
Joakim Andén <janden@math.princeton.edu>
2018-07-23 22:02:37 -0400

    Add many interface in forward `nufft_transform`
    Also, related `nudft*` functions now have many interface support.

--- fourier/nufft/nudft2.m
+++ fourier/nufft/nudft2.m
@@ -17,13 +17,15 @@
 %    nufft2
 
 function im_f = nudft2(im, fourier_pts)
-	if ndims(im) ~= 2
-		error('Input ''im'' must be of the form N1-by-N2.');
+	if numel(asize(im)) < 2
+		error('Input ''im'' must be of the form N1-by-N2-by-L.');
 	end
 
 	if ndims(fourier_pts) > 2 || size(fourier_pts, 1) ~= 2
 		error('Input ''fourier_pts'' must be of the form 2-by-K.');
 	end
+
+	[im, sz_roll] = unroll_dim(im, 3);
 
 	N = size(im, 1);
 
@@ -31,7 +33,9 @@ function im_f = nudft2(im, fourier_pts)
 		error('Only square images supported.');
 	end
 
-	im_f = zeros(size(fourier_pts, 2), 1);
+	L = size(im, 3);
+
+	im_f = zeros(size(fourier_pts, 2), L);
 
 	grid = ceil([-N/2:N/2-1]);
 	[grid_x, grid_y] = ndgrid(grid, grid);
@@ -39,6 +43,8 @@ function im_f = nudft2(im, fourier_pts)
 	pts = [grid_x(:) grid_y(:)]';
 
 	for k = 1:size(fourier_pts, 2)
-		im_f(k) = exp(-i*(fourier_pts(:,k)'*pts))*im(:);
+		im_f(k,:) = exp(-i*(fourier_pts(:,k)'*pts))*im_to_vec(im);
 	end
+
+	im_f = roll_dim(im_f, sz_roll);
 end
--- fourier/nufft/nufft_transform.m
+++ fourier/nufft/nufft_transform.m
@@ -22,15 +22,13 @@ function sig_f = nufft_transform(plan, sig)
 
 	dims = numel(plan.sz);
 
-	sig_sz = size(sig);
+	sig_sz = asize(sig);
 
-	if dims == 1 && sig_sz(2) == 1
-		sig_sz = sig_sz(1);
+	if ~isprefix(plan.sz, sig_sz)
+		error('First dimensions of input ''sig'' must align with plan.sz.');
 	end
 
-	if numel(sig_sz) ~= dims || any(sig_sz ~= plan.sz)
-		error('Input ''sig'' must be of size plan.sz.');
-	end
+	[sig, sz_roll] = unroll_dim(sig, dims+1);
 
 	epsilon = max(plan.epsilon, eps(class(sig)));
 
@@ -43,28 +41,38 @@ function sig_f = nufft_transform(plan, sig)
 			sig_f = nudft3(sig, plan.fourier_pts);
 		end
 	elseif plan.lib_code == 2
-		sig = double(sig(:));
+		sig = double(reshape(sig, prod(plan.sz), []));
 
 		% NUFFT errors if we give epsilon in single precision.
 		epsilon = double(epsilon);
 
+		L = size(sig, 2);
+
+		sig_f = zeros(plan.num_pts, L, class(sig));
+
 		if dims == 1
-			sig_f = nufft1d2(plan.num_pts, ...
-				plan.fourier_pts(1,:), ...
-				-1, epsilon, plan.sz(1), sig);
+			for ell = 1:L
+				sig_f(:,ell) = nufft1d2(plan.num_pts, ...
+					plan.fourier_pts(1,:), ...
+					-1, epsilon, plan.sz(1), sig(:,ell));
+			end
 		elseif dims == 2
-			sig_f = nufft2d2(plan.num_pts, ...
-				plan.fourier_pts(1,:), ...
-				plan.fourier_pts(2,:), ...
-				-1, epsilon, ...
-				plan.sz(1), plan.sz(2), sig);
+			for ell = 1:L
+				sig_f(:,ell) = nufft2d2(plan.num_pts, ...
+					plan.fourier_pts(1,:), ...
+					plan.fourier_pts(2,:), ...
+					-1, epsilon, ...
+					plan.sz(1), plan.sz(2), sig(:,ell));
+			end
 		elseif dims == 3
-			sig_f = nufft3d2(plan.num_pts, ...
-				plan.fourier_pts(1,:), ...
-				plan.fourier_pts(2,:), ...
-				plan.fourier_pts(3,:), ...
-				-1, epsilon, ...
-				plan.sz(1), plan.sz(2), plan.sz(3), sig);
+			for ell = 1:L
+				sig_f(:,ell) = nufft3d2(plan.num_pts, ...
+					plan.fourier_pts(1,:), ...
+					plan.fourier_pts(2,:), ...
+					plan.fourier_pts(3,:), ...
+					-1, epsilon, ...
+					plan.sz(1), plan.sz(2), plan.sz(3), sig(:,ell));
+			end
 		end
 	elseif plan.lib_code == 3
 		if ~isfield(plan, 'nfft_plan_id')
@@ -74,38 +82,56 @@ function sig_f = nufft_transform(plan, sig)
 		sig = double(sig);
 
 		if dims == 2
-			sig = reshape(permute(sig, [2 1]), prod(plan.sz), 1);
+			sig = reshape(permute(sig, [2 1 3]), prod(plan.sz), []);
 		elseif dims == 3
-			sig = reshape(permute(sig, [3 2 1]), prod(plan.sz), 1);
+			sig = reshape(permute(sig, [3 2 1 4]), prod(plan.sz), []);
 		end
 
-		nfft_set_f_hat(plan.nfft_plan_id, sig);
-		nfft_trafo(plan.nfft_plan_id);
-		sig_f = nfft_get_f(plan.nfft_plan_id);
+		L = size(sig, 2);
+
+		sig_f = zeros(plan.num_pts, L, class(sig));
+
+		for ell = 1:L
+			nfft_set_f_hat(plan.nfft_plan_id, sig(:,ell));
+			nfft_trafo(plan.nfft_plan_id);
+			sig_f(:,ell) = nfft_get_f(plan.nfft_plan_id);
+		end
 	elseif plan.lib_code == 4
 		sig = double(sig);
 
 		% FINUFFT errors if we give epsilon in single precision.
 		epsilon = double(epsilon);
 
+		L = size(sig, numel(plan.sz)+1);
+
+		sig_f = zeros(plan.num_pts, L, class(sig));
+
 		if dims == 1
-			sig_f = finufft1d2( ...
-				plan.fourier_pts(1,:), ...
-				-1, epsilon, sig);
+			for ell = 1:L
+				sig_f(:,ell) = finufft1d2( ...
+					plan.fourier_pts(1,:), ...
+					-1, epsilon, sig(:,ell));
+			end
 		elseif dims == 2
-			sig_f = finufft2d2( ...
-				plan.fourier_pts(1,:), ...
-				plan.fourier_pts(2,:), ...
-				-1, epsilon, sig);
+			for ell = 1:L
+				sig_f(:,ell) = finufft2d2( ...
+					plan.fourier_pts(1,:), ...
+					plan.fourier_pts(2,:), ...
+					-1, epsilon, sig(:,:,ell));
+			end
 		elseif dims == 3
-			sig_f = finufft3d2( ...
-				plan.fourier_pts(1,:), ...
-				plan.fourier_pts(2,:), ...
-				plan.fourier_pts(3,:), ...
-				-1, epsilon, ...
-				sig);
+			for ell = 1:L
+				sig_f(:,ell) = finufft3d2( ...
+					plan.fourier_pts(1,:), ...
+					plan.fourier_pts(2,:), ...
+					plan.fourier_pts(3,:), ...
+					-1, epsilon, ...
+					sig(:,:,:,ell));
+			end
 		end
 	end
 
 	sig_f = cast(sig_f, class(sig));
+
+	sig_f = roll_dim(sig_f, sz_roll);
 end
