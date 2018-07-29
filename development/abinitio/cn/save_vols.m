d9558fd44b1091a3975cbb58f07ea7bab5ac394f
Gabi <gabipragier@gmail.com>
2018-07-29 14:48:00 +0300

    inlining measure_deviation_cn

--- development/abinitio/cn/save_vols.m
+++ development/abinitio/cn/save_vols.m
@@ -24,3 +24,15 @@ log_message('Saving abinitio C%d-enforced non-filtered volume under: %s', n_symm
 WriteMRC(vol_no_filt,1,vol_no_filt_file_name);
 
 end
+
+
+function err = measure_deviation_cn(vol,n_symm)
+   
+errs = zeros(1,n_symm);
+for i=1:n_symm-1
+    vol_rot = fastrotate3z(vol,360*i/n_symm);
+    errs(i) = norm(vol(:)-vol_rot(:))/norm(vol(:));
+end
+err = max(errs);
+
+end
