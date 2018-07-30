4af9d2892cd3a06fd8ee8f11d391dda0a6c4eea4
Gabi <gabipragier@gmail.com>
2018-07-30 14:26:05 +0300

    using rots instead of quaternions plus odds and sods

--- development/abinitio/C2/examples/abinitio_c2_no_shifts.m
+++ development/abinitio/C2/examples/abinitio_c2_no_shifts.m
@@ -7,7 +7,7 @@ open_log(0);
 % The MAT file p100_c4_shifted contains 100 projections of size 65x65. The
 % orientations (given as quaternions) used to generate these projections
 % are stored in the the variable "refq". The projection were generated using the following command:
-[projs,refq] = generate_c2_images(100,1/2,65,'GAUSSIAN',0,1);
+[projs,refq] = generate_c2_images(100,1000000000000,65,'SYNTHETIC',0,1);
 
 % load p100_c2_gaussian_no_shifts;
 viewstack(projs,5,5);   % Display the proejctions.
@@ -29,12 +29,12 @@ max_shift  = 0; % number of shifts to consider
 shift_step = 0.5; % the shift step (see Yoel's technical report)
 min_dist_cls = 25; % the minimal distance (in degrees) between two lines in a single images
 clmatrix = cryo_clmatrix_c2_gpu(npf,size(npf,3),1,0,1,min_dist_cls); 
-cl_detection_rate(clmatrix,n_theta,refq);
+cl_detection_rate(clmatrix,n_theta,min_dist_cls,refq);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % step 5  : calculate relative-rotations
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[Rijs,Rijgs,Confijs] = cryo_generateRij(clmatrix,n_theta,refq);
+[Rijs,Rijgs,confijs] = cryo_generateRij(clmatrix,n_theta,refq);
 % Rijs  = cryo_c2_estimate_all_Rijs(clmatrix(:,:,1),n_theta,refq);
 % Rijgs = cryo_c2_estimate_all_Rijs(clmatrix(:,:,2),n_theta,refq);
 
@@ -42,7 +42,7 @@ cl_detection_rate(clmatrix,n_theta,refq);
 % step 6  : inner J-synchronization
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 nImages = size(clmatrix,1);
-[Rijs,Rijgs] = local_sync_J(Rijs,Rijgs,nImages);
+[Rijs,Rijgs,~,isRank1_ijs] = local_sync_J(Rijs,Rijgs,nImages);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % step 7  : outer J-synchronization
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -51,12 +51,14 @@ nImages = size(clmatrix,1);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % step 8  : third rows estimation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-vis  = estimate_third_rows(Rijs,Rijgs,nImages);
+conf = confijs.*isRank1_ijs; 
+is_use_weights = true;
+vis  = estimate_third_rows(Rijs,Rijgs,conf,nImages,is_use_weights);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % step 9  : in-plane rotations angles estimation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-rots = cryo_inplane_rotations(vis,Rijs,Rijgs);
+rots = cryo_inplane_rotations(vis,Rijs,Rijgs,is_use_weights,conf);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % step 10  : Results Analysis
