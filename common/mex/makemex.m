953735b92e65edcebf2960fcb29ec657b7d45ac3
Joakim Andén <janden@flatironinstitute.org>
2018-09-20 10:09:08 -0400

    Fix MEX compilation in `common/mex` under MATLAB

--- common/mex/makemex.m
+++ common/mex/makemex.m
@@ -1,2 +1,7 @@
-mex -lgomp omp_set_num_threads_mx.c
-mex -lgomp omp_get_num_threads_mx.c
+if ~isoctave()
+    mex CFLAGS="\$CFLAGS -fopenmp" -lgomp omp_get_num_threads_mx.c
+    mex -lgomp omp_set_num_threads_mx.c
+else
+    mex omp_get_num_threads_mx.c
+    mex omp_get_num_threads_mx.c
+end
