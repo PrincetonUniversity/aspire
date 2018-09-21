5857cdf10d0b32a12fbc3ed54bdfb4a48e976ee4
Joakim Andén <janden@flatironinstitute.org>
2018-09-21 16:19:59 -0400

    Simplify `common/mex/makemex.m`
    Now that we don't have to have '-fopenmp' sprinked everywhere,
    remove it.

--- common/mex/makemex.m
+++ common/mex/makemex.m
@@ -1,16 +1,7 @@
 if ~isoctave()
-    mex CFLAGS="\$CFLAGS -fopenmp" -lgomp omp_get_max_threads_mx.c
-    mex -lgomp omp_set_num_threads_mx.c
-else
-    env_cflags = getenv('CFLAGS');
-
-    oct_cflags = mkoctfile('-p', 'CFLAGS');
-    oct_cflags = [oct_cflags(1:end-1) ' -fopenmp'];
-
-    setenv('CFLAGS', oct_cflags);
-
     mex -lgomp omp_get_max_threads_mx.c
     mex -lgomp omp_set_num_threads_mx.c
-
-    setenv('CFLAGS', env_cflags);
+else
+    mex -lgomp omp_get_max_threads_mx.c
+    mex -lgomp omp_set_num_threads_mx.c
 end
