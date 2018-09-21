69613317d0aa0c2ab28e8ea9b767be898270fadb
Joakim Andén <janden@flatironinstitute.org>
2018-09-21 15:33:16 -0400

    Update `makemex` for older versions of Octave
    Need to specify that we want OpenMP explicitly.

--- common/mex/makemex.m
+++ common/mex/makemex.m
@@ -2,6 +2,15 @@ if ~isoctave()
     mex CFLAGS="\$CFLAGS -fopenmp" -lgomp omp_get_num_threads_mx.c
     mex -lgomp omp_set_num_threads_mx.c
 else
-    mex omp_get_num_threads_mx.c
-    mex omp_get_num_threads_mx.c
+    env_cflags = getenv('CFLAGS');
+
+    oct_cflags = mkoctfile('-p', 'CFLAGS');
+    oct_cflags = [oct_cflags(1:end-1) ' -fopenmp'];
+
+    setenv('CFLAGS', oct_cflags);
+
+    mex -lgomp omp_get_num_threads_mx.c
+    mex -lgomp omp_set_num_threads_mx.c
+
+    setenv('CFLAGS', env_cflags);
 end
