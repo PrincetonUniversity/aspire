19906e6be77d88ffc23668c2b63a82fcf8a028a2
Joakim Andén <janden@flatironinstitute.org>
2018-11-04 08:45:01 -0500

    Increase SNR in examples
    Makes the resulting MSE more reasonable without increasing the
    number of images which results in greater running time.

--- examples/example_001.m
+++ examples/example_001.m
@@ -6,7 +6,7 @@
 L = 33;                 % Size of images.
 n = 128;                % Number of images.
 
-SNR = 8;                % Signal-to-noise ratio of images.
+SNR = 32;               % Signal-to-noise ratio of images.
 
 %%% Simulate data %%%
 
