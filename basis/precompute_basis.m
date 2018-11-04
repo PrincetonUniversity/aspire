28ea07b4ad96df138fb3753359db2c4f95fa6965
Joakim Andén <janden@flatironinstitute.org>
2018-11-04 08:30:52 -0500

    Remove `expand_t` from basis objects
    These functions are never used and also result in ill-conditioned
    linear systems, so we remove them.

--- basis/precompute_basis.m
+++ basis/precompute_basis.m
@@ -19,14 +19,12 @@ function basis = precompute_basis(basis)
     basis.evaluate = @(v)(precomputed_evaluate(v, basis));
     basis.expand = @(x)(precomputed_expand(x, basis));
     basis.evaluate_t = @(x)(precomputed_evaluate_t(x, basis));
-    basis.expand_t = @(v)(precomputed_expand_t(v, basis));
 
     d = numel(basis.sz);
 
     basis.mat_evaluate = @(V)(mdim_mat_fun_conj(V, 1, d, basis.evaluate));
     basis.mat_expand = @(X)(mdim_mat_fun_conj(X, d, 1, basis.expand));
     basis.mat_evaluate_t = @(X)(mdim_mat_fun_conj(X, d, 1, basis.evaluate_t));
-    basis.mat_expand_t = @(V)(mdim_mat_fun_conj(V, 1, d, basis.expand_t));
 end
 
 function x = precomputed_evaluate(v, basis)
@@ -63,16 +61,4 @@ function v = precomputed_evaluate_t(x, basis)
     v = basis.B'*x;
 
     v = roll_dim(v, sz_roll);
-end
-
-function x = precomputed_expand_t(v, basis)
-    basis_check_evaluate(v, basis);
-
-    [v, sz_roll] = unroll_dim(v, 2);
-
-    x = basis.B'\v;
-
-    x = reshape(x, [basis.sz size(x, 2)]);
-
-    x = roll_dim(x, sz_roll);
 end
