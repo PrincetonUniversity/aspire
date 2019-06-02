4fc5d4da47205a4e1c374d9447ade896d15c7cce
itaysason <itaysason@mail.tau.ac.il>
2019-06-02 13:38:24 +0300

    Change icfft to icfftd. Fix newton iterations.

--- projections/class_average/align/rot_align.m
+++ projections/class_average/align/rot_align.m
@@ -18,30 +18,27 @@ ITER=zeros(P, 1);
 
 for i=1:max_m+1
     C(i, :)=sum(conj(coeff{i}(:, list(1:P, 1))).*coeff{i}(:, list(1:P, 2)), 1);
-end;
+end
 C2=flipud(conj(C(2:end, :)));
-B=real((2*max_m+1)*icfft([C2; C]));
+B=real((2*max_m+1)*icfftd([C2; C], 1));
 [~, rot]=max(B, [], 1);
 rot=(rot-max_m-1)*N_theta/(2*max_m+1);
 
 %%Newton-Raphson method for root finding
+f_prime=@(x) sum(real(sqrt(-1)*repmat(m_list*(pi/180), 1, P).*C(2:end, P).*exp(sqrt(-1)*m_list*x*pi/180)));
+f_prime2=@(x) sum(real((-1)*repmat(m_list.^2*(pi/180)^2, 1, P).*C(2:end, P).*exp(sqrt(-1)*m_list*x*pi/180)));
 x_old=-ones(1, P);
 x_new=rot;
-precision=0.001;
+precision=1e-12;
 iter=0;
 while max(abs(x_new - x_old)) > precision
-    diff=abs(x_new-x_old);
-    id=find(diff>precision);
-    x_old1=x_new(id);
-    f_prime=@(x) sum(real(sqrt(-1)*repmat(m_list*(pi/180), 1, length(id)).*C(2:end, id).*exp(sqrt(-1)*m_list*x*pi/180)));
-    f_prime2=@(x) sum(real((-1)*repmat(m_list.^2*(pi/180)^2, 1, length(id)).*C(2:end, id).*exp(sqrt(-1)*m_list*x*pi/180)));
-    delta=f_prime(x_old1)./f_prime2(x_old1);
-    delta(abs(delta)>10)=sign(delta(abs(delta)>10))*10.*rand(size(find(abs(delta)>10)));
-    x_new(id) = x_old1- delta;
-    clear x_old1
+    max(abs(x_new - x_old))
+    x_old = x_new;
+    delta=f_prime(x_old)./f_prime2(x_old);
+    x_new = x_new - delta;
     iter=iter+1;
     if iter>10, break, end
-end;
+end
 rot=x_new;
 m_list=[0; m_list];
 C=C.*exp(sqrt(-1)*m_list*rot*pi/180);
