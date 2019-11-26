c61173431f5f13a723328036189b1806a42525f5
Yoel Shkolnisky <you@example.com>
2019-11-26 11:32:52 +0200

    Add support for a stack of images.

--- io/visualization/autocontrast.m
+++ io/visualization/autocontrast.m
@@ -1,15 +1,26 @@
 function im2=autocontrast(im)
 
-s=size(im);
-im=im(:);
-frac=0.95; %remove the 5% top values and 5% lowest values;
-rf=(1-frac)/2; % fraction of values to chop from each side of the histogram
+if ndims(im)==3
+    N=size(im,3);
+else
+    N=1;
+end
 
-[~,I]=sort(im);
-imlen=prod(s);
+im2=zeros(size(im));
 
-Ilow=ceil(imlen*rf);
-Ihigh=imlen-Ilow;
-im(I(1:Ilow))=im(I(Ilow));
-im(I(Ihigh:imlen))=im(I(Ihigh));
-im2=reshape(im,s(1),s(2));
+for k=1:N
+    tmp=im(:,:,k);
+    s=size(tmp);
+    
+    frac=0.9; %remove the 5% top values and 5% lowest values;
+    rf=(1-frac)/2; % fraction of values to chop from each side of the histogram
+    
+    [~,I]=sort(tmp);
+    imlen=prod(s);
+    
+    Ilow=ceil(imlen*rf);
+    Ihigh=imlen-Ilow;
+    tmp(I(1:Ilow))=tmp(I(Ilow));
+    tmp(I(Ihigh:imlen))=tmp(I(Ihigh));
+    im2(:,:,k)=reshape(tmp,s(1),s(2));
+end
